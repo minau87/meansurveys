c125870c2351aa814cce952a8f1f06bde969911f
Michel <michel.nauen@stud.hn.de>
2018-08-08 20:32:57 +0200

    Added basic profile functionality

---
+++ client-src/src/app/profile/avatar-upload/avatar-upload.component.ts
@@ -0,0 +1,116 @@
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+
+@Component({
+  selector: 'app-avatar-upload',
+  templateUrl: './avatar-upload.component.html',
+  styleUrls: ['./avatar-upload.component.scss']
+})
+export class AvatarUploadComponent implements OnInit, OnChanges {
+
+  @ViewChild('fileInputField') fileInputField;
+
+  @Input() allowedFileTypes: Array<String>;
+  @Output() fileNotAllowed = new EventEmitter<boolean>();
+  @Output() fileUpload = new EventEmitter<String>();
+  @Output() fileTooBig = new EventEmitter<Number>();
+  @Input() uploadProgress: Number;
+  @Input() maxFileSize: Number;
+
+  fileTypeAllowed: Boolean;
+  fileName: String;
+  imageBase64: String;
+
+  constructor() { }
+
+  ngOnInit() {
+    if (!this.allowedFileTypes) {
+      this.allowedFileTypes = [];
+    }
+    if (!this.maxFileSize) {
+      this.maxFileSize = 0.300;
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.uploadProgress.currentValue === 100) {
+      this.uploadProgress = undefined;
+    }
+  }
+
+  onChooseFile($event) {
+    // If the user cancels the file selection, there are no files, therefore simply return
+    if (!this.fileInputField.nativeElement.files[0]) {
+      return;
+    }
+    // If the file is bigger than the allowed file size
+    if (this.fileInputField.nativeElement.files[0].size) {
+      // Get the file size in MB
+      let fileSize = Math.round((this.fileInputField.nativeElement.files[0].size / 1024 / 1024) * 100) / 100;
+      // Check if the file is bigger than the maximum allowed size
+      if (fileSize > this.maxFileSize) {
+        // If so, simply return and emit an event
+        this.fileTooBig.emit(fileSize);
+        return;
+      }
+    }
+
+    // Check wether or not the type of the selected file is allowed or not
+    if (!this.isFileTypeAllowed()) {
+      this.fileNotAllowed.emit(true);
+      return;
+    }
+    this.readFile();
+  }
+
+  onFileUpload() {
+    // If no image is set, return
+    if (!this.imageBase64) {
+      return;
+    }
+    // Emit file upload event with the image data and reset fields
+    this.fileUpload.emit(this.imageBase64);
+    this.fileName = undefined;
+    this.imageBase64 = undefined;
+  }
+
+  isFileTypeAllowed() {
+    // If the array has a length of zero, all types are allowed, therefore return true
+    if (this.allowedFileTypes.length === 0) {
+      return true;
+    }
+
+    // Get the file name, then the file extension and then it's index in the array of allowed types
+    const fileName: String = this.fileInputField.nativeElement.files[0].name;
+    const fileExt = fileName.split('.').pop();
+    const fileExtIndex = this.allowedFileTypes.indexOf(fileExt);
+
+    if (fileExtIndex !== -1) {
+      // file type is allowed, therefore set as filename which is displayed in the template
+      this.fileName = fileName;
+      // file type is in the allowed file types provided as inputs, therefore return true
+      return true;
+    } else {
+      // file type is not in the allowed file types provided as inputs, therefore return false
+      return false;
+    }
+  }
+
+  readFile(file?: any) {
+    const f = file || this.fileInputField.nativeElement.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', (event: any) => {
+      this.imageBase64 = event.target.result;
+    }, false);
+    reader.readAsDataURL(f);
+  }
+
+}
--- client-src/src/app/profile/profile.component.ts
+++ client-src/src/app/profile/profile.component.ts
@@ -1,15 +1,118 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription, Observable } from 'rxjs';
+import { User, ProfileResponse } from '../models/user.model';
+import { MatSnackBar } from '@angular/material';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  paramsSub: Subscription;
+  getProfileSub: Subscription;
+  uploadPictureSub: Subscription;
+  user: User;
+  fileUploadForm: FormGroup;
+  uploadProgress: Number;
+  maxFileSize = 0.300;
+
+  constructor(
+    private _authService: AuthService,
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _snackbar: MatSnackBar,
+    private _fb: FormBuilder
+  ) {
+    this.fileUploadForm = this._fb.group({
+      file: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+    this.paramsSub = this._route.params.subscribe((params) => {
+      let id = params.id;
+      this.getProfileSub = this._authService.getProfile(id).subscribe((profile: ProfileResponse) => {
+        this.setUser(profile);
+      }, (err) => {
+        setTimeout(() => {
+          console.log(err);
+          this._snackbar.open('An error occured: Status: ' + err.status + ' Message: ' + err.error.msg + '. Try again.', 'Dismiss');
+        }, 0);
+        this._router.navigate(['/login']);
+        return false;
+      });
+    });
+  }
+
+  setUser(profile: ProfileResponse) {
+    this.user = {
+      username: profile.user.username,
+      _id: profile.user._id,
+      email: profile.user.email,
+      name: profile.user.name,
+      password: profile.user.password,
+      profilePicture: profile.user.profilePicture
+    };
+  }
+
+  onFileNotAllowed($event) {
+    console.log($event);
+    this._snackbar.open('Sorry, but you can only choose a .png or .jpg-File.', 'Got it!');
+  }
+
+  onFileUpload($event) {
+    // console.log('Image:', $event);
+    this.uploadPictureSub = this._authService.updateProfilePicture($event).subscribe((event) => {
+      console.log('event:', event);
+      if (event.type === HttpEventType.UploadProgress) {
+        console.log('Upload Progress: ' + Math.round(event.loaded / event.total * 100) + '%');
+        this.uploadProgress = Math.round(event.loaded / event.total * 100);
+      } else if (event.type === HttpEventType.Response) {
+        this.paramsSub = this._route.params.subscribe((params) => {
+          let id = params.id;
+          this.getProfileSub = this._authService.getProfile(id).subscribe((profile: ProfileResponse) => {
+            this.setUser(profile);
+          }, (err) => {
+            setTimeout(() => {
+              console.log(err);
+              this._snackbar.open('An error occured: Status: ' + err.status + ' Message: ' + err.error.msg + '. Try again.', 'Dismiss');
+            }, 0);
+            return false;
+          });
+        });
+        this._snackbar.open('Picture updated!', '', { duration: 3000 });
+      }
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  onFileTooBig($event) {
+    this._snackbar.open(
+      `Sorry, this file is too big (${$event} MB). Choose a file that's not bigger than ${this.maxFileSize} MB.`, 'Got it!'
+    );
+  }
+
+  getImage(binaryImage) {
+    console.log(btoa(binaryImage.data));
+    return 'data:image/png;base64,' + btoa(binaryImage.data);
+  }
+
+  ngOnDestroy() {
+    if (this.getProfileSub) {
+      this.getProfileSub.unsubscribe();
+      this.getProfileSub = undefined;
+    }
+    if (this.uploadPictureSub) {
+      this.uploadPictureSub.unsubscribe();
+      this.uploadPictureSub = undefined;
+    }
   }
 
 }
